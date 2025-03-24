3e81d6e63f1f1ead3cf580de3de68055023ff26e
Dean Divizio <dean@deandivizio.com>
2025-03-24 16:29:55 -0400

    patched labs page out for now

--- app/labs/page.tsx
+++ app/labs/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, {useState, useEffect, Suspense} from "react";
-import { categoryFetch } from "@/actions/InternalLogic";
+// import { categoryFetch } from "@/actions/InternalLogic";
 import { RenderCharts } from "@/utils/ChartRendering";
 
 export default function Labs() {
@@ -9,31 +9,32 @@ export default function Labs() {
   const [error, setError] = useState<string | null>(null);
   const [approvedIDs, setApprovedIDs] = useState() // Used to filter displayed results
 
-  //Grabs Data
-  useEffect(() => {
-    async function dataFetch() {
-      try {
-        const data = await categoryFetch("ClinicalLabs");
-        setData(data); 
-        // console.log(data);
-      } catch (err) {
-        setError('Failed to fetch data');
-        console.error(err);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-      dataFetch();
-  }, []);
+  // //Grabs Data
+  // useEffect(() => {
+  //   async function dataFetch() {
+  //     try {
+  //       const data = await categoryFetch("ClinicalLabs");
+  //       setData(data); 
+  //       // console.log(data);
+  //     } catch (err) {
+  //       setError('Failed to fetch data');
+  //       console.error(err);
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   }
+  //     dataFetch();
+  // }, []);
   return (
     <main className="flex flex-col items-center justify-start pb-12 px-8 xl:px-48">
       <div id="heading" className="mt-8 mb-16">
         <p className="text-center font-thin italic text-4xl">{`Your Labs, by Category`}</p>
       </div>
       <div id="homeCharts">
-        <Suspense>
+        {/* <Suspense>
         <RenderCharts data={data} approvedIDs={approvedIDs} categorize={true} categoryOrder={["Hormones - All", "COMPLETE BLOOD COUNT", "COMPREHENSIVE METABOLIC PANEL", "LIPID PANEL w/ CHOLESTEROL"]}/>
-        </Suspense>
+        </Suspense> */}
+        <p>placeholder</p>
       </div>
     </main>
   );
